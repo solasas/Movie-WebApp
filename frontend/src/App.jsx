b207f83ae4bbb3447bf9fe834e54dc5f899d71fa
sashank solasa <sashanksolasa@gmail.com>
2026-03-04 22:25:43 +0530

    after adding routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import './App.css'
 import MovieCard from './components/MovieCard';
 import Home from './pages/Home';
+import {Routes,Route} from "react-router-dom"
+import Favorites from './pages/Favorites';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
-    <>
-    <Home/>
-    </>
+    <div>
+      <NavBar/>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/favorites' element={<Favorites/>}/>
+      </Routes>
+    </main>
+    </div>
   );
 }
 
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Favorites = () => {
+  return (
+    <div className='favorites-empty '>
+        <h2>No favourite Movies yet</h2>
+        <p>start adding movies to your favourites and they will start appearing here</p>
+    </div>
+  )
+}
+
+export default Favorites
